6f062bdeede1ae88250000f7c2b08fe923dc08dc
SayjinBlackBelt <116757037+sayjinblackbelt@users.noreply.github.com>
2026-09-22 19:34:34 -0300

    feat: add application configuration

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+if not OPENAI_API_KEY:
+    raise ValueError("A variável de ambiente OPENAI_API_KEY não foi configurada.")
